fbbafbee326d563cfa108350f76c0a1c7f88c0f4
YuukiSakihiro <k021g1259@g.neec.ac.jp>
2022-04-13 13:16:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Project1/main.cpp
@@ -0,0 +1,21 @@
+
+#include <Windows.h>
+// ウィンドウプロシージャ
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	// メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		// ウィンドウが破棄された
+	case WM_DESTROY:
+		// OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	// 標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// コンソールへの文字出力
+	OutputDebugStringA("Hello,DirectX!!\n");
+	return 0;
+}
